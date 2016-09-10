f79b29deade63f5593a08128dbf01099939aeb4e
alexey <suharevichalexey@gmail.com>
2016-09-10 18:47:48 +0300

    EntityManagers: A class to serve as Entity creator and iterator.
    Add SetManager and ClassManager.

--- quizlet/client.py
+++ quizlet/client.py
@@ -1,5 +1,6 @@
 import requests
 import tortilla
+from . import entities
 
 BASE_URL = 'https://api.quizlet.com/2.0/'
 
@@ -87,3 +88,66 @@ class QuizletAPI(tortilla.Wrap):
     def user_url(self):
         """ User profile endpoint. """
         return self.users(self.login)
+
+
+class EntityManager:
+    _name = NotImplemented
+    _entity = NotImplemented
+
+    @property
+    def _plural(self):
+        return self._name + 's'
+
+    @property
+    def endpoint(self):
+        return getattr(self.api, self._plural)
+
+    def __init__(self, api):
+        self.api = api
+
+    def new_entity(self, data):
+        """ Creates a new entity from the retrieved data. """
+        return self._entity(data, endpoint=self.endpoint(data['id']))
+
+    def __call__(self, *, id=None, ids=None, **kwargs):
+        """ Returns either user's entities or specified by id(s).
+
+        Arguments:
+            id(:obj:`str`): An entity's id.
+            ids(:obj:`list` of :obj:`str`): An entities' ids.
+        """
+        if ids is not None:
+            return self.endpoint.get(params={self._name + '_ids': ids})
+        elif id is not None:
+            return self.new_entity(self.endpoint(id).get())
+
+        return getattr(self.api.user_url, self._plural).get(params=kwargs)
+
+    def create(self, **kwargs):
+        """ Creates a new entity. """
+        return self.new_entity(self.endpoint.post(data=kwargs))
+
+    def search(self, query, *, max_items=None, **params):
+        """ Searches for the public item. """
+        endpoint = getattr(self.api.search, self._plural)
+        return [self.new_entity(item) for item in
+                endpoint(params=dict(q=query, **params)).items(max_items=max_items)]
+
+    def __iter__(self):
+        return self.endpoint.items()
+
+
+class SetManager(EntityManager):
+    _name = 'set'
+    _entity = entities.Set
+
+    def create(self, title, terms, lang_terms, definitions, lang_definitions):
+        # Gets the idea of what must be present here.
+        return super().create(title=title, terms=terms, lang_terms=lang_terms,
+                              definitions=definitions, lang_definitions=lang_definitions)
+
+
+class ClassManager(EntityManager):
+    _name = 'class'
+    _plural = 'classes'
+    _entity = entities.Class
